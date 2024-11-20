3b34c010cd6e6f21336ff7340a6e235d61de933b
Turphz <cptsprinkelz@gmail.com>
2024-11-20 20:58:34 +1100

    Initial commit

---
+++ src/components/data/menuData.js
@@ -0,0 +1,11 @@
+export const mainMenu = [
+     {link:"#", content:"Home", icon:"lucide:home"},
+     {link:"#about", content:"Join", icon:"octicon:about"},
+     {link:"#portafolio", content:"Watch", icon:"carbon:portfolio"},
+     {link:"#blog", content:"Socials", icon:"mdi:blog"}
+]
+export const menuBlog = [
+     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/#about", content:"Discord", icon:"octicon:person-24"},
+     {link:"/#portafolio", content:"Play", icon:"carbon:portfolio"},
+]
